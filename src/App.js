d2f0a6b9666f6ce9b71e5e6cd028ed900d1ba3e2
maxwellbenton <maxwellbenton@gail.com>
2018-08-20 15:48:38 -0400

    updated tests

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 import React, { Component } from 'react';
 
-import { Navbar } from 'react-bootstrap'
-import CatList from './CatList'
-import { connect } from 'react-redux'
-import * as actions from './actions/catActions.js'
+import { Navbar } from 'react-bootstrap';
+import CatList from './CatList';
+import { connect } from 'react-redux';
+import * as actions from './actions/catActions.js';
 
-export class App extends Component {
-  componentDidMount() {
-    if (this.props.catPics.length === 0) {
-      console.log('in component did mount')
-      this.props.fetchCats()
-    }
-  }
-  render() {
-    return (
-      <div className="App">
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">CatBook</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-        </Navbar>
-        <CatList catPics={this.props.catPics} />
-      </div>
-    );
-  }
+class App extends Component {
+	componentDidMount() {
+		if (this.props.catPics.length === 0) {
+			console.log('in component did mount');
+			this.props.fetchCats();
+		}
+	}
+	render() {
+		return (
+			<div className="App">
+				<Navbar>
+					<Navbar.Header>
+						<Navbar.Brand>
+							<a href="#">CatBook</a>
+						</Navbar.Brand>
+					</Navbar.Header>
+				</Navbar>
+				<CatList catPics={this.props.catPics} />
+			</div>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-  console.log('in map state to props')
-  return {catPics: state.cats.pictures}
+	console.log('in map state to props');
+	return { catPics: state.cats.pictures };
 }
 
 function mapDispatchToProps(dispatch) {
-  return {actions: bindActionCreators(actions, dispatch)}
+	return { actions: bindActionCreators(actions, dispatch) };
 }
 
-
-export const WrapperApp = connect( mapStateToProps, {...actions} )(App)
+export default connect(
+	mapStateToProps,
+	{ ...actions }
+)(App);
--- test/app-component-test.js
+++ test/app-component-test.js
@@ -1,52 +1,55 @@
-import React from 'react'
-import { configure, shallow, mount} from 'enzyme'
-import expect, { createSpy, spyOn, isSpy } from 'expect'
-import thunk from 'redux-thunk'
-import { WrapperApp, App } from '../src/App'
-import CatList from '../src/CatList'
-import sinon from 'sinon'
-import {createStore, applyMiddleware, compose } from 'redux'
-import * as actions from '../src/actions/catActions'
-import configureMockStore from 'redux-mock-store'
-
+import React from 'react';
+import { configure, shallow, mount } from 'enzyme';
+import expect, { createSpy, spyOn, isSpy } from 'expect';
+import thunk from 'redux-thunk';
+import App from '../src/App';
+import CatList from '../src/CatList';
+import sinon from 'sinon';
+import { createStore, applyMiddleware, compose } from 'redux';
+import * as actions from '../src/actions/catActions';
+import configureMockStore from 'redux-mock-store';
 
 import configureStore from 'redux-mock-store';
-import { Provider, connect} from 'react-redux';
-import rootReducer from '../src/reducers/index'
+import { Provider, connect } from 'react-redux';
+import rootReducer from '../src/reducers/index';
+import nock from 'nock';
+import fetch from 'isomorphic-fetch';
 
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adapter() })
+configure({ adapter: new Adapter() });
 
-const images = [{url: "www.example.com/cat1"}, {url: 'www.example.com/cat2'}]
-function setup() {
-  const props = {
-    catPics: images
-  }
-  const mockStore = configureStore([]);
-  const initialState = {cats: {loading: false, cats: []}}
-  const store = mockStore(initialState);
-  const wrapper = shallow(<WrapperApp store={store}/>)
-  return {
-    wrapper
-  }
-}
+const images = [
+	{ url: 'www.example.com/cat1' },
+	{ url: 'www.example.com/cat2' }
+];
 
-function setUpMount() {
-  // const middlewares = [ thunk ]
-  // // const mockStore = configureMockStore(rootReducer, compose(applyMiddleware(middlewares)))({cats: {loading: false, pictures: []}})
+describe('<App/>', function() {
+	it('should use the componentDidMount lifecycle method to fetchCats', function() {
+		sinon.stub(App.prototype, 'componentDidMount');
+		nock('http://localhost:4000')
+			.get('/db')
+			.reply(200, {
+				images: [
+					{ url: 'www.example.com/cat1' },
+					{ url: 'www.example.com/cat2' }
+				]
+			});
+		global.fetch = fetch;
 
-  console.log('before mount in test here')
-  const component = mount(<App catPics={[]}/>)
-  return {component}
-}
+		const props = {
+			catPics: images
+		};
 
-describe('<App/>', function () {
+		const mockStore = configureStore([]);
+		const initialState = { cats: { loading: false, cats: [] } };
+		const store = createStore(rootReducer, applyMiddleware(thunk));
+		const wrapper = mount(
+			<Provider store={store}>
+				<App />
+			</Provider>
+		);
 
-
-  it('should use the componentDidMount lifecycle method to fetchCats', function() {
-    sinon.stub(App.prototype, 'componentDidMount');
-    const {component } = setUpMount()
-    expect(App.prototype.componentDidMount.calledOnce).toEqual(true);
-  })
+		expect(App.prototype.componentDidMount.calledOnce).toEqual(true);
+	});
 });
